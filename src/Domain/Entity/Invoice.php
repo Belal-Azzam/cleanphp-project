2e9f01b5951d391ee8a122a519d7d5f2c1060ba9
= <=>
2018-11-30 20:05:42 +0200

    added all entities

---
+++ src/Domain/Entity/Invoice.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: belal
+ * Date: 2018-11-30
+ * Time: 8:04 PM
+ */
+namespace CleanPhp\Invoicer\Domain\Entity;
+class Invoice {
+    protected $order;
+    protected $invoiceDate;
+    protected $total;
+    public function getOrder() {
+        return $this->order;
+    }
+    public function setOrder(Order $order) {
+        $this->order = $order;
+        return $this;
+    }
+    public function getInvoiceDate() {
+        return $this->invoiceDate;
+    }
+    public function setInvoiceDate(\DateTime $invoiceDate) {
+        $this->invoiceDate = $invoiceDate;
+        return $this;
+    }
+    public function getTotal() {
+        return $this->total;
+    }
+    public function setTotal($total) {
+        $this->total = $total;
+        return $this;
+    }
+}
+    ?>
---
+++ src/Domain/Entity/Order.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: belal
+ * Date: 2018-11-30
+ * Time: 8:03 PM
+ */
+class Order
+{
+    protected $customer;
+    protected $orderNumber;
+    protected $description;
+    protected $total;
+
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer($customer)
+    {
+        $this->customer = $customer;
+        return $this;
+    }
+    public function getOrderNumber() {
+        return $this->orderNumber;
+    }
+    public function setOrderNumber($orderNumber) {
+        $this->orderNumber = $orderNumber;
+        return $this;
+    }
+    public function getDescription() {
+        return $this->description;
+    }
+    public function setDescription($description) {
+        $this->description = $description;
+        return $this;
+    }
+    public function getTotal() {
+        return $this->total;
+    }
+    public function setTotal($total) {
+        $this->total = $total;
+        return $this;
+    }
+}
+?>
